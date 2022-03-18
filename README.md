8eae577ef17a863517ba76a15f11f29ff8fb310d
Kevin He <kevin.he@salesforce.com>
2022-03-17 17:46:31 -0700

    ssr

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This repository is used to experiment the new React 18 APIs.
 
+https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html
+
 ### Get started
 
 ```
--- index.js
+++ index.js
@@ -1,8 +1,27 @@
+import React from "react";
 import express from "express";
+import { renderToPipeableStream } from "react-dom/server";
 
 const PORT = 3000;
 
 const app = express();
+
+const Component = () => <div>React18</div>;
+
+app.get("*", (req, res) => {
+  const { pipe, abort } = renderToPipeableStream(<Component />, {
+    // bootstrapScripts: [assets["main.js"]],
+    onShellReady() {
+      res.statusCode = 200;
+      res.setHeader("Content-type", "text/html");
+      pipe(res);
+    },
+    onError(x) {
+      console.error(x);
+    },
+  });
+  return;
+});
 
 app.listen(PORT, () => {
   console.log("express app running: http://localhost:3000");
